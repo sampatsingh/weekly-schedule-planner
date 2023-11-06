41fc28ba0ea862b519db2c5aac8a13f9571c3a35
Sampat Singh <smpt.sinh@gmail.com>
2023-11-06 21:48:25 +0530

    Enhance appointment API test cases with improved comments and remove this.timeout

--- test/CreateAppointment.test.js
+++ test/CreateAppointment.test.js
@@ -2,6 +2,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../server"); // Import your Express server from server.js
 const expect = chai.expect;
+const Appointment = require("../models/Appointment"); // Import the Appointment model
 
 chai.use(chaiHttp);
 
@@ -16,10 +17,10 @@ describe("Appointments API", () => {
         description: "Annual check-up",
       })
       .end((err, res) => {
-        // Assertion 1: Check the HTTP response status code
+        // Assertion 1: Verify the HTTP response status code is 201 (Created)
         expect(res).to.have.status(201);
 
-        // Assertion 2: Check the response body is an object
+        // Assertion 2: Ensure the response body is an object
         expect(res.body).to.be.an("object");
 
         // Assertion 3: Check the 'title' property in the response body
@@ -27,5 +28,44 @@ describe("Appointments API", () => {
 
         done(); // Signal that the test is complete
       });
+  });
+
+  it("should retrieve all upcoming appointments", (done) => {
+    chai
+      .request(app)
+      .get("/appointments")
+      .end((err, res) => {
+        // Assertion 1: Verify the HTTP response status code is 200 (OK)
+        expect(res).to.have.status(200);
+
+        // Assertion 2: Ensure the response body is an array
+        expect(res.body).to.be.an("array");
+
+        done();
+      });
+  });
+
+  it("should delete an appointment", (done) => {
+    // First, you need to create an appointment for testing the delete operation
+    const newAppointment = new Appointment({
+      title: "Test Appointment",
+      date: "2023-12-31T15:30:00",
+      description: "Testing delete operation",
+    });
+
+    newAppointment.save((error, savedAppointment) => {
+      chai
+        .request(app)
+        .delete(`/appointments/${savedAppointment._id}`)
+        .end((err, res) => {
+          // Assertion 1: Verify the HTTP response status code is 200 (OK)
+          expect(res).to.have.status(200);
+
+          // Assertion 2: Check the response message
+          expect(res.body).to.have.property("message", "Appointment deleted");
+
+          done();
+        });
+    });
   });
 });
